c33717cede99a6bf4b804793130cbaa7ee3548c5
Rob Cole <rob-cole@hotmail.co.uk>
2021-12-05 21:27:21 +0000

    Modify nav response

--- assets/java/button.js
+++ assets/java/button.js
@@ -17,3 +17,13 @@
 //   document.body.scrollTop = 0; // For Safari
 //   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 // }
+<script>
+function myFunction() {
+  var x = document.getElementById("main-nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
+</script>
